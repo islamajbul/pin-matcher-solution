462db862cc19a04725f6bd92f71afb04c4b9c9a2
islamajabul <ajabulmd1@gmail.com>
2023-02-18 23:14:04 +0800

    done

--- js/app.js
+++ js/app.js
@@ -41,6 +41,27 @@ document.getElementById('calculator').addEventListener('click',function(event){
     else{
         const newTypedNumber = previousTypedNumber + number;
         typedNumberField.value = newTypedNumber;
+    }   
+})
+
+document.getElementById('verify-pin').addEventListener('click',function(){
+    const displayPinField = document.getElementById('display-pin');
+    const currentPin = displayPinField.value;
+
+    const typedNumberField = document.getElementById('typed-numbers');
+    const newTypedNumber = typedNumberField.value;
+
+    const pinSuccessMessage = document.getElementById('pin-success');
+    const pinFailureMessage = document.getElementById('pin-failure');
+
+    if(newTypedNumber === currentPin){
+        pinSuccessMessage.style.display = 'block';
+        pinFailureMessage.style.display = 'none';
     }
-    
+
+    else{
+        
+        pinFailureMessage.style.display = 'block';
+        pinSuccessMessage.style.display = 'none';
+    }
 })
